4545e7e9669a3879c06e7143825976cb261dfa7c
abdulla <abdullaabdullaev0803@gmail.com>
2023-03-22 12:41:00 +0300

    finish the logic of game, add required components

--- index.js
+++ index.js
@@ -1,30 +1,85 @@
-const game = document.querySelector(".game")
-const gameBox = document.querySelector("#gameBox")
-const test = document.getElementsByTagName("div")
-console.log(gameBox);
-console.log(game)
-console.log(test);
+const Player = (name, tool, value) => ({ name, tool, value })
+const player1 = Player("josh", "x", 1)
+const player2 = Player("mosh", "o", 2)
 const GameBoard = (() => {
-    const store = ["x", "0", "x", "0", "x", "0", "x", "0", "x"];
-    const render = function () {
-        for (let i = 0; i < store.length; i++) {
-            console.log(store[i]);
+    const store = [];
+    const boxes = document.getElementsByClassName("boxes")
+    // const player1 = Player("josh", toolInput, 22)
+    // const player2 = Player("mosh", toolInput, 22)
+    const btnScore = () => {
+        const header = document.querySelector("header")
+        const askForTool = document.createElement("button")
+        askForTool.textContent = "  weapon"
+        header.appendChild(askForTool)
+        const chooseTool = () => {
+            const xbtn = document.createElement("button")
+            const obtn = document.createElement("button")
+            header.appendChild(xbtn)
+            header.appendChild(obtn)
+            xbtn.textContent = "x"
+            obtn.textContent = "o"
         }
-    }
+        askForTool.addEventListener("click", chooseTool)
 
+        // function chooseTool() {
+        //     const container = this.textContent
+        //     console.log(container);
+        // }
+        // xbtn.addEventListener("click", chooseTool)
+        // obtn.addEventListener("click", chooseTool)
+    }
+    const render = () => {
+    }
     const createDivs = () => {
         for (let i = 0; i < 9; i++) {
             const box = document.createElement("div")
-            // gameBox.appendChild(box)
+            box.classList = "boxes"
+            gameBox.appendChild(box)
         }
     }
-    return { render, createDivs }
+    const changeBoxes = () => {
+        let number = 0
+        for (let i = 0; i < 9; i++) {
+            boxes[i].addEventListener("click", addText)
+        }
+        function evenOrOdd() {
+            if (number % 2 == 0) {
+                return "false"
+            } return "true"
+        }
+        function endOfGame() {
+            if (number == 1) {
+                console.log("over");
+                return false
+            }
+        }
+        function addText() {
+            if (number == 9) {
+                return false
+            }
+            if (this.textContent) {
+                return false;
+            } console.log(1);
+            number++
+            if (evenOrOdd() == "false") {
+                this.textContent = player1.tool
+            }
+            else if (evenOrOdd() == "true") {
+                this.textContent = player2.tool
+            }
+            store.push(this.textContent)
+            console.log(store);
+        }
+    }
+
+    return { render, createDivs, btnScore, changeBoxes }
 })()
 
 GameBoard.createDivs()
-
-
 // GameBoard.render()
+GameBoard.btnScore()
+GameBoard.changeBoxes()
+
 // const Player = (name, tool) => {
     // console.log(this.name);
     // return { name, tool }
